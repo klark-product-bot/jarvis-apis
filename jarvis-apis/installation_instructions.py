9b98c10a9551c1fe78946a049edec19d66cce664
never2average <priyesh.sriv2017@gmail.com>
2020-04-24 09:28:43 +0530

    Request URL Switch

--- jarvis-apis/installation_instructions.py
+++ jarvis-apis/installation_instructions.py
@@ -11,7 +11,8 @@ def listgitrepos(token):
     data = json.loads(tokenValidator[1].to_json())
     username = data["github_creds"]["git_username"]
     personal_token = data["github_creds"]["git_personal_token"]
-    url = "https://api.github.com/user/repos"
+    org_name = data["github_creds"]["org_name"]
+    url = "https://api.github.com/user/orgs/{}/repos".format(org_name)
     base64string = base64.encodestring('{}/token:{}'.format(username, personal_token).encode()).decode().replace("\n", "")
     resp = requests.get(url, headers={"Authorization": "Basic "+base64string})
     if resp.status_code != 200:
@@ -29,5 +30,9 @@ def listgitrepos(token):
     
 
 def helpwithinstallation(token, data):
-    # projectname, docker=False, OS="Ubuntu"
-    pass
+    projectname = data["projectname"]
+    needDocker = data["docker"]
+    if not needDocker:
+        OS = data["os"]
+    else:
+        OS = "Ubuntu"
