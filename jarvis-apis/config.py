18601b14e2973e094504ddb2c7401af2f625820f
never2average <priyesh.sriv2017@gmail.com>
2020-04-24 09:03:19 +0530

    Config

--- jarvis-apis/config.py
+++ jarvis-apis/config.py
@@ -3,15 +3,15 @@ import jwt
 import json
 
 def validateToken(token):
-    # try:
-    decoded_token = jwt.decode(
-        token,
-        "YOUR-KEY-GOES-HERE"
-    )["email"]
-    user = User.objects.get(email=decoded_token)
-    return True, user
-    # except:
-    #     return False, {
-    #         'statusCode': 401,
-    #         "message": "InvalidCredentials"
-    #     }
+    try:
+        decoded_token = jwt.decode(
+            token,
+            "YOUR-KEY-GOES-HERE"
+        )["email"]
+        user = User.objects.get(email=decoded_token)
+        return True, user
+    except:
+        return False, {
+            'statusCode': 401,
+            "message": "InvalidCredentials"
+        }
